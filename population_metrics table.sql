b30f390a47526945350ade353dcc9fe5ed7c975e
mayfiete <terry.e.g.mayfield@gmail.com>
2021-07-15 16:34:41 -0400

    feat: adding population_metrics table

---
+++ population_metrics table.sql
@@ -0,0 +1,8 @@
+
+
+CREATE TABLE [census].[population_metrics](
+	[category] [varchar](250) NULL,
+	[population] [varchar](250) NULL,
+	[race_code] [varchar](25) NULL,
+	[state_code] [varchar](25) NULL
+); 
