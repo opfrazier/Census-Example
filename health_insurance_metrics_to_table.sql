7797774af8ed47220eb9a372ef5354613899ca60
Olivia Frazier <84338743+opfrazier@users.noreply.github.com>
2021-07-20 17:56:13 +0000

    Add files via upload

---
+++ health_insurance_metrics_to_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [census_data_2].[health_insurance_metrics](
+	[category] [varchar](250) NULL,
+	[number_insured] [varchar](250) NULL,
+	[number_uninsured] [varchar](25) NULL,
+	[poverty_ratio][varchar](25) NULL,
+	[age] [varchar](25) NULL,
+	[year] [varchar](25) NULL, 
+	[sex] [varchar](25) NULL,
+	[race] [varchar](25) NULL
+); 
+
